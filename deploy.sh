abf7f8f9efb1d403069923440829dbf7dec8a958
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-21 18:27:19 -0300

    agrega dokerfile

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+sudo docker stop mail-service-container 2>/dev/null
+sudo docker rm mail-service-container 2>/dev/null
+
+sudo docker build -t mail-service .
+
+sudo docker run \
+           --restart always \
+           -d -p 8084:8084 \
+           --env-file .env \
+           --network appx \
+           --name mail-service-container mail-service
